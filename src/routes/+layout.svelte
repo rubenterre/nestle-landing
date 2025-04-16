8817129a448739fe100a9c4ce5abb8459cffc54c
daninv36 <danielnegreiravilar@gmail.com>
2025-04-16 13:20:13 +0200

    Update: crear header

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Header from '$lib/components/Header.svelte';
+</script>
+
+<Header></Header>
+
+<style>
+	@font-face {
+		font-family: Nunito;
+		src: url('styles/fonts/Nunito-VariableFont_wght.ttf');
+	}
+
+	:global(body) {
+		font-family: Nunito;
+	}
+</style>
