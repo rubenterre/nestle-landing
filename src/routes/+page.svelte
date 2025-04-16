574b56e9cf9d89d98f06eb0bf9ddb10f0e415988
lola <lupitaforreiro@gmail.com>
2025-04-16 13:35:19 +0200

    update: titulo y ranting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,38 @@
+<script>
+	import Titulo from "$lib/components/Titulo.svelte";
+
+</script>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<Titulo></Titulo>
+
+<style>
+
+:global(*) {
+  margin: 0;
+  padding: 0;
+  outline: 0;
+  box-sizing: border-box;
+}
+@font-face{
+    font-family: "Nunito";
+    src: url(./styles/fonts/NunitoSans.ttf);
+}
+
+:global(body) {
+ background: white;
+ color: white ;
+ -webkit-font-smoothing: antialiased;
+}
+
+:global(body, input, button) {
+ font-size: 14px;
+ font-family:"Nunito";
+}
+
+:global(button) {
+ cursor: pointer;
+}
+
+</style>
