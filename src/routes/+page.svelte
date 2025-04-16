e1d4ae960fbe920fa0d75f1e6d70ef2aa541ef2e
LauraAmparo <tialaury@gmail.com>
2025-04-16 13:42:00 +0200

    update:Descripción

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import Description from "$lib/components/Description.svelte";
+
+</script>
+
+<Description></Description>
