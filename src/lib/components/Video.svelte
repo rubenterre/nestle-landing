854a3a7d3f13e7999d0f559201ffd644753e5920
tasberbie200666 <i.hate.broccoli80@gmail.com>
2025-04-22 10:33:23 +0200

    create a Video compopnent

---
+++ src/lib/components/Video.svelte
@@ -0,0 +1,18 @@
+<div class="video">
+	<iframe
+		width="719px"
+		height="414px"
+		src="https://www.youtube.com/embed/5YjXOsIgZGM?si=xBVxOSN630mbBMOo"
+		title="YouTube video player"
+		frameborder="0"
+		allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+		referrerpolicy="strict-origin-when-cross-origin"
+		allowfullscreen
+	></iframe>
+</div>
+
+<style>
+    .video{
+        height: 414px;
+    }
+</style>
